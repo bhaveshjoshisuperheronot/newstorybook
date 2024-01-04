edb4a612d181399fbe2a31032653dc84920a47eb
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2024-01-04 07:15:05 +0000

    Templates

--- stories/Card/Card.js
+++ stories/Card/Card.js
@@ -7,14 +7,18 @@ export const Card = ({
     children='I am a card',
     color = 'primary',
     size = 'sm',
+    isClickable,
+    isDraggable,
 }) => {
-    return <div className={`card color-${color} size-${size}`}>{children}</div>
+    return <div className={`card color-${color} size-${size} ${isClickable? "is-clickable":""} ${isDraggable? "is-draggable":""}`}>{children}</div>
 }
 
 Card.propTypes = {
     children: PropTypes.node.isRequired,
     color: PropTypes.oneOf(options.colors),
     size: PropTypes.oneOf(options.sizes),
+    isClickable: PropTypes.bool,
+    isDraggable: PropTypes.bool,
 }
 
 export default Card
--- stories/Card/Card.stories.js
+++ stories/Card/Card.stories.js
@@ -7,12 +7,27 @@ export default {
     component: Card
 }
 
-export const Default = () => <Card />
+const Template = (args) => <Card {...args} />
+const ListTemplate = ({items, ...args}) => items.map((item, index) => <Card key={index} {...args} {...item}/>)
 
-export const Colors = () => options.colors.map((color, index) => {
-    return <Card key={index} color={color} />
-})
+export const Default = Template.bind({})
 
-export const Sizes = () => options.sizes.map((size, index) => {
-    return <Card key={index} size={size} />
-})
+export const Clickable = Template.bind({})
+Clickable.args = {
+    isClickable: true
+}
+
+export const Draggable = Template.bind({})
+Draggable.args = {
+    isDraggable: true
+}
+
+export const Colors = ListTemplate.bind({})
+Colors.args = {
+    items: options.colors.map(color => ({color}))
+}
+
+export const Sizes = ListTemplate.bind({})
+Sizes.args = {
+    items: options.sizes.map(size => ({size}))
+}
